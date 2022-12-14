6b485366bc7ef080f088fcb87f49f7002f38868b
Michael Machado <machado.m3@icloud.com>
2022-12-14 08:15:28 -0300

    first commit Ignite Lab

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { PrismaService } from './prima.service';
+import { HttpModule } from './http.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [AppController],
+  providers: [PrismaService],
+})
+export class AppModule {}
